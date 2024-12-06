1fe2779e3242348819d0165a32b0fbce0b044fda
kjh <kimjaehwi1212@gmail.com>
2024-12-06 21:42:11 +0900

    세 번째

--- script/script.js
+++ script/script.js
@@ -1,17 +1,34 @@
 $(document)
-.on('click', '#tree, .close', popToggle)
+.on('click', '.left2, .close', popToggle)
 const image = document.getElementById('tree');
-const letter = document.querySelector('.')
-
-image.addEventListener('click', e =>{
-    const userInput = prompt("편지내용을 입력하세요")
-
-    if(userInput !== null) {
-        alert(userInput)
-    }
-})
-
+const popup = document.querySelector('.left2')
+const scroll = document.querySelector('.container')
 
 function popToggle(){
     $('.paper_bg').toggle()
 }
+
+popup.addEventListener('click', e => {
+    scroll.scrollTop = 0
+    lock.style.overflow = 'hidden';
+})
+
+
+function accessToGeo (position) {
+    console.log(position)
+}
+
+function askForLocation () {
+    navigator.geolocation.getCurrentPosition(accessToGeo)
+}
+
+askForLocation();
+
+
+Kakao.init('f7d1c40b420e91c5d77a70b4d6badd09')
+Kakao.isInitialized()
+
+Kakao.Auth.authorize({
+    redirectUri: 'http://localhost:5500/webapp/html/index.html',
+    scope: 'profile_nickname'
+  });
